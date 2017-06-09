1c343ff74abf655b67d80218e53cf96179340432
Elena Williams <git@elena.net.au>
2017-06-10 03:55:06 +1000

    feat: Add subledger abstract classes
    **TADA**
    
    Much consideration has brought about these classes. Will it be worth it?
    Only time will tell.

---
+++ subledgers/models.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+from django.db import models
+from decimal import Decimal
+from ledgers.models import Transaction
+
+
+# ~~~~~~~ ======= ######################################### ======== ~~~~~~~ #
+
+# Abstract Classes for Subledgers
+#
+# See detailed discussion:
+# http://admin-accounting.blogspot.com.au/2017/06/subledger-generalisation-and-specifics.html
+
+# ~~~~~~~ ======= ######################################### ======== ~~~~~~~ #
+
+
+class Entry(models.Model):
+
+    ## *** ABSTRACT CLASS ***
+
+    """ Generalised in case more fields are necessary for subjedgers.
+
+    For use with at least: Sales, Expenses
+    """
+
+    transaction = models.OneToOneField(Transaction)
+
+    class Meta:
+        abstract = True
+
+
+class Invoice(Entry):
+
+    ## *** ABSTRACT CLASS ***
+
+    """ For use with at least: Creditors Invoices, Debtors Invoices
+
+    7 legal components of an invoice: the words "invoice", date, seller,
+    seller abn, what, tax (and what applied to), total value, (due date)
+
+    Transaction contains:
+    - Date
+    - Reference
+    - Value
+    - Note
+    - User
+
+    """
+
+    due_date = models.DateField(null=True)
+
+    invoice_number = models.CharField(max_length=128)
+
+    order_number = models.CharField(max_length=128, blank=True, default="")
+
+    reference = models.CharField(max_length=128, blank=True, default="")
+
+    gst_total = models.DecimalField(max_digits=19, decimal_places=2, default=Decimal('0.00'))
+
+
+    class Meta:
+        abstract = True
+
+
+class Payment(models.Model):
+
+    ## *** ABSTRACT CLASS ***
+
+    """ Payment is transaction with 2 lines:
+    DR/CR bank account
+    DR/CR creditor/liability or debtor/asset
+
+    Rather than fields that matter, methods matter.
+    """
+
+    transaction = models.OneToOneField(Transaction)
+
+    class Meta:
+        abstract = True

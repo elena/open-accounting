d946490c1217b231bdde88b303f7888689ea29dc
Elena Williams <git@elena.net.au>
2017-06-23 15:45:30 +1000

    feat: Add ReST pagination

--- project/settings/base.py
+++ project/settings/base.py
@@ -133,3 +133,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# 3rd Party Apps
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 20
+}

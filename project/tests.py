528dba393a87efcdd7c1cdcbc9b9a9b344b7e127
Elena Williams <git@elena.net.au>
2017-06-23 15:28:03 +1000

    test: Improve project tests

--- project/tests.py
+++ project/tests.py
@@ -1,6 +1,7 @@
+# -*- coding: utf-8 -*-
 import unittest
 from django.contrib.auth.models import User
-from django.test import TestCase, Client
+from django.test import TestCase  # , Client
 
 
 class HomeTestAnonymousUser(TestCase, unittest.TestCase):
@@ -14,13 +15,30 @@ class HomeTestAnonymousUser(TestCase, unittest.TestCase):
         self.assertTemplateUsed(response, 'base.html')
 
 
-class HomeTestAuthenticatedUser(TestCase, unittest.TestCase):
+class HomeTestActiveUser(TestCase, unittest.TestCase):
 
     def setUp(self):
-        user = User.objects.create_user('test_user', 'test@example.com', '1234')
-        user.is_staff=True
+        # user = User.objects.create_user(
+        #     'test_active_user', 'test@example.com', '1234')
+        self.client.login(username='test_active_user', password='1234')
+
+    def test_home_loads(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_uses_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'base.html')
+
+
+class HomeTestStaffUser(TestCase, unittest.TestCase):
+
+    def setUp(self):
+        user = User.objects.create_user(
+            'test_staff_user', 'test@example.com', '1234')
+        user.is_staff = True
         user.save()
-        self.client.login(username='test_user', password='1234')
+        self.client.login(username='test_staff_user', password='1234')
 
     def test_home_loads(self):
         response = self.client.get('/')

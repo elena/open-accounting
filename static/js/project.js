e157c6eebeddfcd7f20ddd7b702e6a5541f319a5
Elena Williams <git@elena.net.au>
2017-06-23 13:49:16 +1000

    style: Tune css/js for display BankTransaction

--- static/js/project.js
+++ static/js/project.js
@@ -24,8 +24,12 @@
     $(document).ready(function() {
         $('#dataTable').DataTable({
             "lengthMenu": [
-                [10, 25, 50, 100, 250, -1],
-                [10, 25, 50, 100, 250, "All"]]
+                [20, 50, 100, 250, -1],
+                [20, 50, 100, 250, "All"]],
+              "columnDefs": [
+                  { "targets": 0, "type": "html" }
+              ],
+             "dom": '<"top"i>rt<"bottom"flp><"clear">'
         });
     });
 

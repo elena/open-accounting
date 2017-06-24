a3302e6aa655e505f4e6eeddb2f37d9736b2ae67
Elena Williams <git@elena.net.au>
2017-06-24 17:55:23 +1000

    trivial: Move around bank urls to make more sense

--- subledgers/bank_reconciliations/urls.py
+++ subledgers/bank_reconciliations/urls.py
@@ -10,28 +10,29 @@ urls = [
     url(r'^$',
         views.BankAccountListView.as_view(),
         name='bank-reconciliation-index'
-    ),
+        ),
+
+    # Reconcile listview
+    url(r'^(?P<account>[0-9\-]+)/$',
+        # views.BankReconciliationView.as_view(),
+        views.bank_reconciliation,
+        name='bank-reconciliation'
+        ),
 
     # Statement upload
     url(r'^statements/$',
         views.add_statements,
         name='bank-statement-upload'
-    ),
+        ),
 
+    # API urls
     url(r'^api/(?P<pk>[0-9]+)/$',
         views.BankTransactionViewSet.as_view({
             'get': 'retrieve',
             'put': 'update',
             'post': 'update',
             'patch': 'partial_update'})
-    ),
-
-    # Reconcile listview
-    url(r'^(?P<account>[0-9\-]+)/$',
-        views.bank_reconciliation,
-        name='bank-reconciliation'
-    ),
-
+        ),
 ]
 
 urlpatterns = [url(r'^', include(urls, namespace='bank-reconciliations'))]

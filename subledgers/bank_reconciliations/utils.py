531ab3740445b2c9b8db7e9af516fcf0596580a7
Elena Williams <git@elena.net.au>
2017-06-25 08:33:11 +1000

    refactor: Remove old workings from BankTransaction upload

--- subledgers/bank_reconciliations/utils.py
+++ subledgers/bank_reconciliations/utils.py
@@ -13,22 +13,6 @@ duplicate transactions.
 This is impossible checking any individual line of a statement. It is
 possible for details which are identical in every way to occur twice on a
 bank statement.
-
-Therefore we will check each *day* and be prejudiced against any
-misalignment.
-
-To do this we will:
-1. import data and create dictionary with: date, value, line_dump, balance
-2. we will get the oldest and newest date
-3. we will iterate through each day and check to if:
-  - dict obj v. bank transaction
-  - sum values
-  - are equal
-  if so: continue. (perhaps do a count of transactions here also)
-  else:
-    check which has the greater COUNT of transactions.
-    if DB is greater: ignore dict
-    if dict >=: blow away the day in DB and reimport per this dict
 """
 
 
@@ -41,80 +25,11 @@ def import_bank_statement(data):
     # 1. generate list of **kwargs based on lines from statement
     list_kwargs = preprocessor(data['input_data'])
 
-    # 2. get oldest and newest dates to iterate through
-    dates = [x['date'] for x in list_kwargs]
-    working_date, max_date = min(dates), max(dates)
+    for kwargs in list_kwargs:
+        new = BankTransaction(bank_account=bank, **kwargs)
+        new.save()
+        results.append(new)
 
-    # 3. iterate through date, check integrity against existing objects
-    while working_date <= max_date:
-        sum_value_obj = BankTransaction.objects.filter(
-            date=working_date).aggregate(Sum('value'))
-        sum_value_kwargs = sum(
-            [x['value'] for x in list_kwargs if x['date'] == working_date])
-        list_day_obj = BankTransaction.objects.filter(date=working_date)
-        list_day_kwargs = [x for x in list_kwargs if x['date'] == working_date]
-
-        if list_day_obj.count() == 0:
-            # Nothing exists in database.
-            # There are no objects, create new objects.
-            for kwargs in list_day_kwargs:
-                new = BankTransaction(bank_account=bank, **kwargs)
-                new.save()
-                results.append(new)
-            working_date += timedelta(1)
-            continue
-        elif sum_value_obj['value__sum'] == sum_value_kwargs \
-                and len(list_day_obj) == len(list_day_kwargs):
-            # Seems to be perfect match in database.
-            # Sum and count both match, continue.
-            # @@TODO still edge cases here.
-            working_date += timedelta(1)
-            continue
-        else:
-            # Mismatch:
-            # Firstly sift out any object which may have already been matched
-            for obj in list_day_obj:
-                if obj.transaction:
-                    # if object is already matched try and find match in
-                    # list_kwargs and remove it from this list
-                    match_list = [
-                        x for x in list_kwargs if x['value'] == obj.value]
-                    if len(match_list) == 0:
-                        # obj missed out on import
-                        continue
-                    elif len(match_list) == 1:
-                        # 1 match easy to remove
-                        list_day_kwargs.remove(match_list[0])
-                    else:
-                        # if multiple matches try and get a better match
-                        match_list2 = [
-                            x for x in match_list if x['balance'] == obj.balance]  # noqa
-                        if len(match_list2) == 0:
-                            match_list3 = [x for x in match_list
-                                           if x['description'] == obj.description]  # noqa
-                            if match_list3:
-                                list_day_kwargs.remove(match_list3[0])
-                            else:
-                                list_day_kwargs.remove(match_list[0])
-                        elif len(match_list2) == 1:
-                            list_day_kwargs.remove(match_list2[0])
-                        else:
-                            match_list3 = [x for x in match_list2
-                                           if x['description'] == obj.description]  # noqa
-                            if match_list3:
-                                list_day_kwargs.remove(match_list3[0])
-                            else:
-                                list_day_kwargs.remove(match_list2[0])
-                else:
-                    # the newer/greater statement should be correct, delete
-                    # anything not already matched.
-                    obj.delete()
-            # should have reduced kwargs list, now create new obj.
-            for kwargs in list_day_kwargs:
-                new = BankTransaction(bank_account=bank, **kwargs)
-                new.save()
-                results.append(new)
-            working_date += timedelta(1)
     return results
 
 

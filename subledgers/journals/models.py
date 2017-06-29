e6abb9ee997473677d83d8fabdd4be4a1efcbb0e
Elena Williams <git@elena.net.au>
2017-06-29 17:40:08 +1000

    feat: Add to JournalEntry relation Entity
    Not 100% that this is a good idea, but might be, so providing
    flexibility for it overall.

--- subledgers/journals/models.py
+++ subledgers/journals/models.py
@@ -1,8 +1,13 @@
 # -*- coding: utf-8 -*-
+from django.db import models
 from ..models import Entry
 
 
 class JournalEntry(Entry):
+
+    entity = models.ForeignKey(
+        'entities.Entity',
+        default="", blank=True, null=True)
 
     class Meta:
         verbose_name_plural = "journal entries"

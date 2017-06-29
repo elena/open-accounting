7127a8cd291ee297a6998dba0afce1ad5066c340
Elena Williams <git@elena.net.au>
2017-06-29 16:46:05 +1000

    minor: Add BankEntryAdmin for date sorting

--- subledgers/bank_reconciliations/admin.py
+++ subledgers/bank_reconciliations/admin.py
@@ -3,6 +3,11 @@ from django.contrib import admin
 from .models import BankTransaction, BankEntry
 
 
+class BankEntryAdmin(admin.ModelAdmin):
+    list_display = ['transaction', 'bank_transaction']
+    list_filter = ['transaction__date']
+
+
 class BankTransactionAdmin(admin.ModelAdmin):
     list_display = ['bank_account', 'date', 'description',
                     'additional', 'value']
@@ -10,4 +15,4 @@ class BankTransactionAdmin(admin.ModelAdmin):
 
 
 admin.site.register(BankTransaction, BankTransactionAdmin)
-admin.site.register(BankEntry)
+admin.site.register(BankEntry, BankEntryAdmin)

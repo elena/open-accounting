3ac798ab9391654a3fc36ea15b25198a809402a5
Elena Williams <git@elena.net.au>
2017-07-02 22:11:34 +1000

    minor: Add admin for Sales

--- subledgers/sales/admin.py
+++ subledgers/sales/admin.py
@@ -1,7 +1,7 @@
-# from django.contrib import admin
+from django.contrib import admin
 
 
-# from .models import Sale
+from .models import Sale
 
 
-# admin.site.register(Sale)
+admin.site.register(Sale)

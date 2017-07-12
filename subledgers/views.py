ea92ed8c2c034c9579c788577d599ff96a25ac2f
Elena Williams <git@elena.net.au>
2017-07-12 16:41:35 +1000

    feat: Enforce logged in for upload view.

--- subledgers/views.py
+++ subledgers/views.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # from django.forms import formset_factory
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.utils.module_loading import import_string
 
 from ledgers import utils
@@ -10,32 +10,38 @@ from subledgers.models import Entry
 
 def upload_view(request, import_function=None):
 
-    template_name = 'subledgers/upload_form.html'
-    context_data = {}
+    if request.user.is_authenticated:
 
-    if request.method == 'POST':
-        form = UploadForm(request.POST)
-        context_data['dump'] = request.POST
-
-        if request.POST.get('object_name'):
-            data = Entry.dump_to_objects(
-                request.POST['input_data'],
-                request.user, request.POST['object_name'],
-                live=int(request.POST['live']),
-            )
-        else:
-            data = Entry.dump_to_objects(
-                request.POST['input_data'], request.user,
-                live=int(request.POST['live']),
-            )
         template_name = 'subledgers/upload_form.html'
-        context_data['results'] = data
+        context_data = {}
+
+        if request.method == 'POST':
+            form = UploadForm(request.POST)
+            context_data['dump'] = request.POST
+
+            print(request.POST)
+
+            if request.POST.get('object_name'):
+                data = Entry.dump_to_objects(
+                    request.POST['input_data'],
+                    request.user, request.POST['object_name'],
+                    live=int(request.POST['live']),
+                )
+            else:
+                data = Entry.dump_to_objects(
+                    request.POST['input_data'], request.user,
+                    live=int(request.POST['live']),
+                )
+            template_name = 'subledgers/upload_form.html'
+            context_data['results'] = data
+            return render(request, template_name, context_data)
+        else:
+            form = UploadForm()
+
+        context_data['form'] = form
         return render(request, template_name, context_data)
     else:
-        form = UploadForm()
-
-    context_data['form'] = form
-    return render(request, template_name, context_data)
+        return redirect("/admin/login/?next=/upload/")
 
 
 def dump_view(request):

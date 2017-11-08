cf0788cee2cbe61efc512cd406a856f86b853834
Elena Williams <git@elena.net.au>
2017-11-09 10:36:38 +1100

    fix: Ensure tax returns something pertinent and useful
    Small but important exclusions that were problematic.

--- subledgers/taxes/AU/utils.py
+++ subledgers/taxes/AU/utils.py
@@ -4,7 +4,7 @@ from ledgers.models import Account
 from . import settings
 
 
-def process_tax(cls, kwargs):
+def process_tax(cls, kwargs, lines):
     """ Just a check that GST is defined if required.
 
     If subledger requires GST error will be raised if GST
@@ -14,7 +14,7 @@ def process_tax(cls, kwargs):
     - GST tb account being used
     - `GST total` column/ `gst_total` key in kwargs
     """
-    object_settings = settings.OBJECT_SETTINGS
+    object_settings = settings.OBJECT_SETTINGS[utils.get_source_name(cls)]
 
     # @@ TODO facilitate other taxes and surcharges.
     if object_settings.get('is_GST'):
@@ -26,7 +26,7 @@ def process_tax(cls, kwargs):
 
         # First check if GST_CR_ACCOUNT or GST_DR_ACCOUNT lines exist
         gst_allocated = False
-        for line in kwargs['lines']:
+        for line in lines:
             if Account.get_account(line[0]) == \
                Account.get_account(settings.GST_DR_ACCOUNT)\
                or Account.get_account(line[0]) == \
@@ -38,15 +38,19 @@ def process_tax(cls, kwargs):
         # - set acccount using `is_tb_account_DR/CR` to get `GST_DR/CR_ACCOUNT`
         # Note: the value +/- is correct to subledger. May not be correct for
         #       the tb. +/- corrected to tb set in different process.
+
         if not gst_allocated:
             # note: correct GST account, abs value
             # fix dr/cr +/- in next process, not here.
-            if object_settings.get('is_tb_account_DR'):
-                kwargs['lines'].append((
-                    settings.GST_DR_ACCOUNT,
-                    utils.make_decimal(kwargs['gst_total'])))
-            if object_settings.get('is_tb_account_CR'):
-                kwargs['lines'].append((
-                    settings.GST_CR_ACCOUNT,
-                    utils.make_decimal(kwargs['gst_total'])))
-    return kwargs
+
+            if not kwargs['gst_total'] in ['', None, 0]:
+                if object_settings.get('is_tb_account_DR'):
+                    lines.append((
+                        settings.GST_DR_ACCOUNT,
+                        utils.make_decimal(kwargs['gst_total'])))
+                if object_settings.get('is_tb_account_CR'):
+                    lines.append((
+                        settings.GST_CR_ACCOUNT,
+                        utils.make_decimal(kwargs['gst_total'])))
+
+    return lines
